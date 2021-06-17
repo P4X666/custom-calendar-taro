e518880832b040b4b658cc3988f60af957a2dc77
彭彭 <50615274+P4X666@users.noreply.github.com>
2021-06-17 10:15:10 +0800

    完善utils中ts支持

--- src/components/Calendar/utils.ts
+++ src/components/Calendar/utils.ts
@@ -45,7 +45,7 @@ export const calcWeekDay = (current: number, add: number) => {
  * @param date 属于目标月份的Date对象
  * @param startDay 一行的起点  比如以周一为起点 此时startDay = 1，以周日为起点，此时startDay = 0
  */
-export const getDateListByMonth = (date: Date, startDay: number) => {
+export const getDateListByMonth = (date: Date, startDay: number):Array<CalendarDateInfo> => {
   const month = date.getMonth();
   const year = date.getFullYear();
   /** 一周的最后一天 */
@@ -121,11 +121,11 @@ export const getDateListByMonth = (date: Date, startDay: number) => {
  * @param date 属于目标星期的Date对象
  * @param startDay 一行的起点  比如以周一为起点 此时startDay = 1，以周日为起点，此时startDay = 0
  */
-export const getDateListByWeek = (date: Date, startDay: number) => {
+export const getDateListByWeek = (date: Date, startDay: number): Array<CalendarDateInfo> => {
   date.setDate(date.getDate() - ((date.getDay() - startDay + 7) % 7));
   /** 一周的最后一天 */
   const weekEndDay = calcWeekDay(startDay, 6);
-  let result: Array<object> = [];
+  let result: Array<CalendarDateInfo> = [];
   while (date.getDay() !== weekEndDay) {
     result.push({
       date: date.getDate(),
